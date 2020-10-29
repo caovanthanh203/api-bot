09d54cb50d8b9942a3d8bb2a8ce8dcc8259a1cda
Cao Van Thanh <caovanthanh203@gmail.com>
2020-10-29 15:52:38 +0700

    add sort cuuho

--- overrideCuuho/override.py
+++ overrideCuuho/override.py
@@ -1,21 +1,38 @@
 import json
+import datetime
 
 data = {}
 
-# def extract_case(json):
-#     try:
-#         return int(json["attributes"]['Confirmed'])
-#     except KeyError:
-#         return 0
+def extract_case(json):
+    try:
+        return json["timestamp"]
+    except KeyError:
+        return 0
+
+def timestamp(string):
+	timestr = string[:-6]
+	try:
+		datetimeObj = datetime.datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%S")
+		return (datetimeObj-datetime.datetime(1970,1,1)).total_seconds()
+	except ValueError:
+		try:
+			datetimeObj = datetime.datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%S.%f")
+			return (datetimeObj-datetime.datetime(1970,1,1)).total_seconds()
+		except ValueError:
+			return 0
 
 with open('_input.json') as inputFile:
     inputData = json.load(inputFile)
     data = inputData["results"];
+    for index, item in enumerate(data):
+    	item["timestamp"] = timestamp(item["update_time"])
+    	data[index] = item
 
 with open('_target.json') as targetFile:
     targetData = json.load(targetFile)
     targetData["cuuho"] = data;
 
-# data["features"].sort(key=extract_case, reverse=True)
+# print(parse(data[1]["update_time"]).datetime())
+targetData["cuuho"].sort(key=extract_case, reverse=True)
 with open('_output.json', 'w') as outputFile:
     json.dump(targetData, outputFile)
